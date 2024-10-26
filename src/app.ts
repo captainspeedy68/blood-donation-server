f3a2f54dc7d4ac2590353f59175f57c29a1ca2fe
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-10-26 21:25:13 +0600

    lint prettier setup done

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-import { Request, Response } from "express"
+import { Request, Response } from 'express'
 
-const express = require('express')
+import express from "express";
 const app = express()
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
-  })
+  res.send('Hello World!')
+})
 
 export default app;
