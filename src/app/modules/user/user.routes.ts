3623bee093a5639ec4ef8db4d7a1f6b291fc61d7
Bishal Acharya <82877265+captainspeedy68@users.noreply.github.com>
2024-12-07 17:45:02 +0600

    adding client and donor posting with as user

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,7 +1,16 @@
 import express from "express";
 import { UserControllers } from "./user.controller";
+import { clientCreationValidationSchema } from "../client/client.validation";
+
+import validationMiddleware from "../../middlewares/validateRequests";
+import { donorCreationValidationSchema } from "../donor/donor.validation";
+
+
 const router = express.Router();
 
-router.post("/create-client", UserControllers.createClient);
+
+router.post("/create-client", validationMiddleware(clientCreationValidationSchema), UserControllers.createClient);
+
+router.post("/create-donor",validationMiddleware(donorCreationValidationSchema), UserControllers.createDonor);
 
 export const UserRoutes = router;
