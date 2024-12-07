e97292d82d4a319e52b7053445817d1656295755
Bishal Acharya <82877265+captainspeedy68@users.noreply.github.com>
2024-12-07 18:32:52 +0600

    agrregation used for get donor with queries

--- src/app/modules/donor/donor.service.ts
+++ src/app/modules/donor/donor.service.ts
@@ -1,5 +1,6 @@
 import { Donor } from './donor.model';
-
+// import redis from 'redis';
+// const client = redis.createClient();
 // Service to fetch all donors from the database
 const getAllDonorsFromDB = async () => {
     console.log('Fetching all donors...');
@@ -10,11 +11,24 @@ const getAllDonorsFromDB = async () => {
 // Service to fetch donors based on query parameters (like bloodGroup, division, district)
 const getDonorsByQueryFromDB = async (bloodGroup: string, division: string, district: string) => {
     try {
-        const result = await Donor.find({
-            bloodGroup,
-            'presentAddress.division': division,
-            'presentAddress.district': district,
-        });
+        // const result = await Donor.find({
+        //     bloodGroup,
+        //     'presentAddress.division': division,
+        //     'presentAddress.district': district,
+        // });
+        const result = await Donor.aggregate([
+            { $match: { 
+                "presentAddress.division": division,
+                "presentAddress.district": district,
+                bloodGroup: bloodGroup
+            }},
+            {
+                $group: {
+                    _id: "$presentAddress.district",
+                    totalDonors: { $sum: 1 }
+                }
+            }
+        ]);
         return result;
     } catch (error) {
         console.error('Error fetching donors:', error);
@@ -22,7 +36,30 @@ const getDonorsByQueryFromDB = async (bloodGroup: string, division: string, dist
     }
 };
 
+const getDonorsWithPagination = async (page: number, limit: number) => {
+    const result = await Donor.find()
+        .skip((page - 1) * limit)
+        .limit(limit);
+    return result;
+}
+
+// const getDonorsWithCache = async () => {
+//     const cacheKey = "donorsCache";
+    
+//     // Check if data is in cache
+//     client.get(cacheKey, async (err, cachedData) => {
+//         if (cachedData) {
+//             return JSON.parse(cachedData); // Return cached data
+//         } else {
+//             const donors = await Donor.find();
+//             client.setex(cacheKey, 3600, JSON.stringify(donors)); // Cache for 1 hour
+//             return donors;
+//         }
+//     });
+// }
+
 export const DonorServices = {
     getAllDonorsFromDB,
     getDonorsByQueryFromDB,
+    getDonorsWithPagination
 };
