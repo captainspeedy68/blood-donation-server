81d987f58930d50c37b7257443651fc579b51809
Bishal Acharya <82877265+captainspeedy68@users.noreply.github.com>
2024-12-07 17:47:16 +0600

    user service and controller changes

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,23 +1,35 @@
-import httpStatus  from 'http-status';
+import httpStatus from 'http-status'
 import { NextFunction, Request, RequestHandler, Response } from 'express'
 import { UserServices } from './user.service'
-import sendResponse from '../../utils/sendResponse';
-import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse'
+import catchAsync from '../../utils/catchAsync'
 
 const createClient: RequestHandler = catchAsync(async (req, res, next) => {
   const { password, client } = req.body
   // console.log(client);
-    const result = await UserServices.createClientIntoDB(password, client);
+  const result = await UserServices.createClientIntoDB(password, client)
 
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Client created successfully',
-      data: result,
-    })
-  }
-)
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Client created successfully',
+    data: result,
+  })
+})
+
+const createDonor: RequestHandler = catchAsync(async (req, res, next) => {
+  const { password, donor } = req.body;
+  const result = await UserServices.createDonorIntoDB(password, donor);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Donor Created Successfully",
+    data: result
+  })
+})
 
 export const UserControllers = {
-  createClient
+  createClient,
+  createDonor
 }
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,6 +1,8 @@
 import config from '../../config'
 import { TClient } from '../client/client.interface'
 import { Client } from '../client/client.model'
+import { TDonor } from '../donor/donor.interface'
+import { Donor } from '../donor/donor.model'
 import { TUser } from './user.interface'
 import { User } from './user.model'
 
@@ -13,7 +15,7 @@ const createClientIntoDB = async (password: string, clientData: TClient) => {
   //   set role
   userData.role = 'client'
 
-  userData.id = '6242'
+  userData.id = '6245'
 
   const result = await User.create(userData)
   if (Object.keys(result).length) {
@@ -27,6 +29,22 @@ const createClientIntoDB = async (password: string, clientData: TClient) => {
   // console.log(result);
 }
 
+
+const createDonorIntoDB = async(password: string, donorData: TDonor) =>{
+  const userData: Partial<TUser> = {};
+  userData.password = password || (config.default_password as string);
+  userData.role = 'client';
+  userData.id = '6329';;
+  const result = await User.create(userData);
+  if(Object.keys(result).length){
+    donorData.id = result.id;
+    donorData.user = result._id;
+    const newDonor = await Donor.create(donorData);
+    return newDonor;
+  }
+}
+
 export const UserServices = {
   createClientIntoDB,
+  createDonorIntoDB
 }
